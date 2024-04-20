40fef6a0307c6b0f792e0eb2bce92c9aaf01b3f4
theowod <152944741+theowod@users.noreply.github.com>
2024-04-19 21:13:16 -0400

    netflix repo

---
+++ netflix/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Home from "./pages/Home/Home";
+import React from  "react";
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ netflix/src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
+import Banner from "../../components/Banner/Banner";
+import RowLIst from "../../components/Rows/RowList/RowList";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <RowLIst />
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
